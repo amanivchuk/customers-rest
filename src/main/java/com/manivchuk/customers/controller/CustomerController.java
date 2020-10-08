f2f16b903e0536cba460a2fc87edaf39d8f7c513
Aleksandr Manivchuk <amanivhuk@gmail.com>
2020-10-08 15:28:58 +0300

    refactoring

--- src/main/java/com/manivchuk/customers/controller/CustomerController.java
+++ src/main/java/com/manivchuk/customers/controller/CustomerController.java
@@ -25,7 +25,7 @@ import java.net.MalformedURLException;
 import java.util.*;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200", "*"})
 @RestController
 @RequestMapping("/api")
 @Setter(onMethod_ = @Autowired)
